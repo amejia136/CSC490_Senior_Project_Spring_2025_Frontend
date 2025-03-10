de7c71072ecaf2c167f6d2f67687f1ececcede1d
Andrea <mayerlyy@yahoo.com>
2025-03-10 11:41:32 -0400

    -- Implemented routers, login can now go directly to home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import Home from "./Components/Home/Home";
-//import Login from "./Components/Login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
+import Login from "./Components/Login/Login";
 
 
 const App = () => {
     return (
-        <>
-            <Navbar/>
-            <Home/>
-            <Main/>
-            <Footer/>
-        </>
-    )
-}
+        <Router>
+            <Routes>
+                {/* Login Page */}
+                <Route path="/" element={
+                    <>
+                        <Login />
+                    </>
+                } />
+                {/* Home Page */}
+                <Route path="/home" element={
+                    <>
+                        <Navbar />
+                        <Home />
+                        <Main />
+                        <Footer />
+                    </>
+                } />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
