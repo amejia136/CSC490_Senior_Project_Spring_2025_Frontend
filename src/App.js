d60b6dff3391f4edf3af2a85f12f4170ccb686dc
Andrea <mayerlyy@yahoo.com>
2025-03-11 12:49:18 -0400

    -- Fixed issues with Profile and Achievements that it gets small

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React from "react";
-import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
 import Navbar from "./Components/Navbar/Navbar";
-//import Home from "./Components/Home/Home";
+import Home from "./Components/Home/Home";
 import Main from "./Components/Main/Main";
-//import Footer from "./Components/Footer/Footer";
-//import Login from "./Components/Login/Login";
+import Footer from "./Components/Footer/Footer";
+import Login from "./Components/Login/Login";
 import Profile from "./Components/Profile/Profile";
 import Achievements from "./Components/Achievements/Achievements";
-import Footer from "./Components/Footer/Footer";
 
 const App = () => {
     return (
-        <BrowserRouter> {/*so profile/achievements show*/ } >
-            <><Navbar />
-                <Achievements />
-            </>
-        </BrowserRouter>
+        <Router>
+            <Routes>
+
+                {/* Login Page */}
+                <Route path="/login" element={
+                    <>
+                        <Login />
+                    </>
+                } />
+
+                {/* Home Page */}
+                <Route path="/" element={
+                    <>
+                        <Navbar />
+                        <Home />
+                        <Main />
+                        <Footer />
+                    </>
+                } />
+
+                {/* Profile Page */}
+                <Route path="/profile" element={
+                    <>
+                        <Navbar />
+                        <Profile />
+                    </>
+                } />
+                {/* Achievements Page */}
+                <Route path="/achievements" element={
+                    <>
+                        <Navbar />
+                        <Achievements />
+                    </>
+                } />
+            </Routes>
+        </Router>
     );
 };
 
